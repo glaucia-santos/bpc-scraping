e517d7db7e2af8f3f4c4b0552aaa06c66e1412cd
glaucia-santos <glauaasantos@gmail.com>
2025-05-06 00:57:47 -0300

    Adiciona endpoint /api/status/

--- backend/bpc_app/urls.py
+++ backend/bpc_app/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
+
+def status_view(request):
+    return JsonResponse({'status': 'online'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/status/', status_view),
 ]
